52ee9337e1c1b922968d8c83fd059e43456e8c90
yurley <yurs.ksf1@gmail.com>
2021-11-23 20:40:43 -0500

    add validation email and blockig multiple bad password

--- valid_password.py
+++ valid_password.py
@@ -15,25 +15,25 @@ X email: yurs.ksf1@gmai.com  pass: DiplomaDopython1234
 """
 
 CORREOS_REGISTRADOS = [
-    ['albueno1186@gmail.com', 'albueno1186'],
-    ['basp0606@gmail.com', 'basp0606'],
-    ['camilonavarro14@gmail.com', 'camilonavarro14'],
-    ['cristo_blanco@hotmail.com', 'cristo_blanco'],
-    ['santiagogalvis771@gmail.com', 'santiagogalvis771'],
-    ['deyvyjava@hotmail.com', 'deyvyjava'],
-    ['santarosasur@hotmail.com', 'santarosasur'],
-    ['elkinleoardo08@gmail.com', 'elkinleoardo08'],
-    ['e.bernal0322@gmail.com', 'bernal0322'],
-    ['erika.pita1982@hotmail.com', 'pita1982'],
-    ['taverap@gmail.com', 'taverap'],
-    ['Fjsr1098@gmail.com', 'Fjsr1098'],
-    ['jelirodriguez23@gmail.com', 'jelirodriguez23'],
-    ['Calderonnjhonj@gmail.com', 'Calderonnjhonj'],
-    ['milton_mote@hotmail.com', 'milton_mote'],
-    ['Jdpalomino31@gmail.com', 'Jdpalomino31'],
-    ['Juank_fortin10@hotmail.com', 'Juank_fortin10'],
-    ['Meyerlafm@gmail.com', 'Meyerlafm'],
-    ['jhofrgo@gmail.com', 'jhofrgo'],
+    ['albueno1186@gmail.com', 'albueno1186', 0],
+    ['basp0606@gmail.com', 'basp0606', 0],
+    ['camilonavarro14@gmail.com', 'camilonavarro14', 0],
+    ['cristo_blanco@hotmail.com', 'cristo_blanco', 0],
+    ['santiagogalvis771@gmail.com', 'santiagogalvis771', 0],
+    ['deyvyjava@hotmail.com', 'deyvyjava', 0],
+    ['santarosasur@hotmail.com', 'santarosasur', 0],
+    ['elkinleoardo08@gmail.com', 'elkinleoardo08', 0],
+    ['e.bernal0322@gmail.com', 'bernal0322', 0],
+    ['erika.pita1982@hotmail.com', 'pita1982', 0],
+    ['taverap@gmail.com', 'taverap', 0],
+    ['Fjsr1098@gmail.com', 'Fjsr1098', 0],
+    ['jelirodriguez23@gmail.com', 'jelirodriguez23', 0],
+    ['Calderonnjhonj@gmail.com', 'Calderonnjhonj', 0],
+    ['milton_mote@hotmail.com', 'milton_mote', 0],
+    ['Jdpalomino31@gmail.com', 'Jdpalomino31', 0],
+    ['Juank_fortin10@hotmail.com', 'Juank_fortin10', 0],
+    ['Meyerlafm@gmail.com', 'Meyerlafm', 0],
+    ['jhofrgo@gmail.com', 'jhofrgo', 0],
 ]
 
 
@@ -41,12 +41,20 @@ def validar(email, password):
     for ee in CORREOS_REGISTRADOS:
         validacion_email = ee[0].upper() == email.upper()
         if validacion_email:
+            if ee[2] >= 3:
+                print('contrasena bloqueada. contacta con el admin')
+                return False
             validacion_password = ee[1] == password
             if validacion_password:
+                ee[2] = 0
                 return True
+            else:
+                ee[2] = ee[2] + 1
 
     return False
 
+def validar_email(email):
+    return '@' in email
 
 def run():
     message = """
@@ -56,13 +64,14 @@ def run():
     while True:
         print(message)
         email = input("ingresa email: ")
-        password = input("ingresa password: ")
-
-        if validar(email, password):
-            print("te logueaste correctamente")
-            break
+        if validar_email(email):
+            password = input("ingresa password: ")
+            if validar(email, password):
+                print("te logueaste correctamente")
+            else:
+                print("te no te pudiste loguear, intenta nuevamente")
         else:
-            print("te no te pudiste loguear, intenta nuevamente")
+            print("email no valido, debe tener un @")
 
     print('fin codigo')
 
