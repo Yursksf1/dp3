cfd1269839d7bf3bfb57b9e687a83841170c55ed
yurley <yurs.ksf1@gmail.com>
2021-11-25 20:30:29 -0500

    refactor model

--- django_uno/myapp/admin.py
+++ django_uno/myapp/admin.py
@@ -2,18 +2,14 @@ from django.contrib import admin
 
 # Register your models here.
 
-from django.contrib import admin
 from myapp.models import Propietario
+from myapp.models import Mascota
 
 class PropietarioAdmin(admin.ModelAdmin):
     pass
-
-admin.site.register(Propietario, PropietarioAdmin)
-
-
-from myapp.models import Mascota
 
 class MascotaAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Mascota, MascotaAdmin)
+admin.site.register(Propietario, PropietarioAdmin)
