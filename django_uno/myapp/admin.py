6a83c97a34e3551454cdc038cf6310c2effe8f83
yurley <yurs.ksf1@gmail.com>
2021-11-26 20:32:21 -0500

    improve model admin with models

--- django_uno/myapp/admin.py
+++ django_uno/myapp/admin.py
@@ -9,7 +9,10 @@ class PropietarioAdmin(admin.ModelAdmin):
     pass
 
 class MascotaAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'name', 'propietario')
+    list_filter = ('propietario',)
+    search_fields = ('name',)
+    ordering = ('id',)
 
 admin.site.register(Mascota, MascotaAdmin)
 admin.site.register(Propietario, PropietarioAdmin)
